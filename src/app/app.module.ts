9cff241522ad5161f126faeee6029fb273bc7934
MiguelGN <114272562+MiguelGN@users.noreply.github.com>
2022-10-11 10:57:36 -0500

    avance de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FuncionesComponent } from './funciones/funciones.component';
+import { SidebarComponent } from './funciones/sidebar/sidebar.component';
+import { NavbarComponent } from './funciones/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionesComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
